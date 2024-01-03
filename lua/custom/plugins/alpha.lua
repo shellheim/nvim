7450d674653b4f37ff59d25053c97ac17fe77e61
shellheim <116480892+shellheim@users.noreply.github.com>
2024-01-03 22:11:30 +0530

    pluralize and remove duplicate telescope options

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -22,9 +22,9 @@ return {
     }
     dashboard.section.buttons.val = {
       dashboard.button('n', ' > New File', '<cmd>ene<CR>'),
-      dashboard.button('f', '󰱼 > Find File', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('f', '󰱼 > Find Files', '<cmd>Telescope find_files<CR>'),
       dashboard.button('r', ' > Find Recent Files', '<cmd>Telescope oldfiles<CR>'),
-      dashboard.button('p', ' > Find Recent Files', '<cmd>Telescope project<CR>'),
+      dashboard.button('p', ' > Find Projects', '<cmd>Telescope project<CR>'),
       dashboard.button('c', ' > Configuration', '<cmd>edit ~/.config/nvim/init.lua<CR>'),
       dashboard.button('q', ' > Quit NVIM', '<cmd>qa<CR>'),
     }
