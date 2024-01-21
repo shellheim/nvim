8f22d3a3316ecab290ecbf26208b428245c5d797
shellheim <116480892+shellheim@users.noreply.github.com>
2024-01-21 22:11:17 +0530

    Replac formatter/linter 'clinet' 1. Remove none-ls 2. Add conform.nvim

---
+++ lua/custom/plugins/conform.lua
@@ -0,0 +1,35 @@
+return {
+
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  config = function()
+    local conform = require 'conform'
+
+    -- Reduce boilerplate by looping over the langs prettier supports.
+    local prettier_langs = {
+      'javascript',
+      'javascriptreact',
+      'typescript',
+      'typescriptreact',
+      'markdown',
+      'css',
+      'html',
+    }
+    local opts = {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        sh = { 'shellcheck', 'shfmt' },
+        json = { 'jq' },
+        jsonc = { 'jq' },
+      },
+      format_on_save = {
+        timeout_ms = 1000,
+        lsp_fallback = true,
+      },
+    }
+    for _, lang in ipairs(prettier_langs) do
+      opts.formatters_by_ft[lang] = { 'prettier' }
+    end
+    conform.setup(opts)
+  end,
+}
