238e48c6badff67c6f3d95d3ffba36df98bf6ff7
shellheim <116480892+shellheim@users.noreply.github.com>
2024-01-31 18:48:43 +0530

    Formatting changes

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -10,22 +10,26 @@ return {
     harpoon:setup()
     -- REQUIRED
 
-    vim.keymap.set('n', '<leader>ak', function()
-      harpoon:list():append()
-    end)
     vim.keymap.set('n', '<leader>l', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    vim.keymap.set('n', '<leader>ak', function()
+      harpoon:list():append()
     end)
 
     vim.keymap.set('n', '<leader>1', function()
       harpoon:list():select(1)
     end)
+
     vim.keymap.set('n', '<leader>2', function()
       harpoon:list():select(2)
     end)
+
     vim.keymap.set('n', '<leader>3', function()
       harpoon:list():select(3)
     end)
+
     vim.keymap.set('n', '<leader>4', function()
       harpoon:list():select(4)
     end)
@@ -34,6 +38,7 @@ return {
     vim.keymap.set('n', '<C-S-P>', function()
       harpoon:list():prev()
     end)
+
     vim.keymap.set('n', '<C-S-N>', function()
       harpoon:list():next()
     end)
