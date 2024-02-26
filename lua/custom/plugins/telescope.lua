19021e7141324779d99747482a8a52ee46d57b5d
shellheim <116480892+shellheim@users.noreply.github.com>
2024-02-26 12:20:03 +0530

    Formatting changes

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,30 +1,23 @@
 return {
-
   'nvim-telescope/telescope.nvim',
   event = 'VeryLazy',
   branch = '0.1.x',
-  dependencies = {
+  dependencies = { 	
     'nvim-lua/plenary.nvim',
     -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available. Make sure you have the system
     -- requirements installed.
     {
       'nvim-telescope/telescope-fzf-native.nvim',
-      -- NOTE: If you are having trouble with this installation,
-      --       refer to the README for telescope-fzf-native for more instructions.
       build = 'make',
       cond = function()
         return vim.fn.executable 'make' == 1
       end,
     },
-    {
       'nvim-telescope/telescope-project.nvim',
-    },
   },
 
 
   config = function()
-    -- [[ Configure Telescope ]]
-    -- See `:help telescope` and `:help telescope.setup()`
     require('telescope').setup {
       defaults = {
         mappings = {
