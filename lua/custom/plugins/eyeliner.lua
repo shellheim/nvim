498516c190ba30129f43223ad5a312e7e0ad3184
shellheim <116480892+shellheim@users.noreply.github.com>
2024-02-26 12:27:09 +0530

    Formatting changes

--- lua/custom/plugins/eyeliner.lua
+++ lua/custom/plugins/eyeliner.lua
@@ -1,13 +1,12 @@
 return {
-  'jinh0/eyeliner.nvim',
-
-  config = function()
-    local eyeliner = require 'eyeliner'
-    eyeliner.setup {
-      highlight_on_key = true,
-      dim = false,
-    }
-    vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg = '#faa700', bold = true, underline = true })
-    vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg = '#faf600', underline = true })
-  end,
+	'jinh0/eyeliner.nvim',
+	config = function()
+		local eyeliner = require 'eyeliner'
+		eyeliner.setup {
+			highlight_on_key = true,
+			dim = false,
+		}
+		vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg = '#faa700', bold = true, underline = true })
+		vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg = '#faf600', underline = true })
+	end,
 }
