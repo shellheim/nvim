b07bf5e63c6f2129414bf3f24d7ebe8921933b65
shellheim <116480892+shellheim@users.noreply.github.com>
2024-09-26 16:26:38 +0530

    move lsp keymaps to keymaps file

--- lua/custom/keymaps/keymaps.lua
+++ lua/custom/keymaps/keymaps.lua
@@ -8,6 +8,21 @@ key.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 key.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 key.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- LSP
+
+-- find
+key.set('n', '<leader>fs', '<cmd>lua vim.lsp.buf.references()<cr>', { desc = '[F]ind All Reference[s]' })
+key.set('n', '<leader>k', '<cmd>lua vim.lsp.buf.hover()<cr>', { desc = 'Hover Symbol Information' })
+
+key.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', { desc = '[G]oto [D]efinition' })
+key.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', { desc = '[G]oto [D]eclration' })
+key.set('n', 'td', '<cmd>lua vim.lsp.buf.type_definition()<cr>', { desc = '[T]ype [D]efinition' })
+key.set('n', 'gi', '<cmd>lua vim.lsp.buf.type_definition()<cr>', { desc = '[G]oto [I]mplementation' })
+
+key.set('n', '<leader>rs', '<cmd>lua vim.lsp.buf.rename()<cr>', { desc = '[R]ename [S]ymbol' })
+
+key.set('n', '<leader>ca', 'vim.lsp.buf.code_action', { desc = '[C]ode [A]ction' })
+
 -- Diagnostic keymaps
 key.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 key.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
@@ -15,12 +30,15 @@ key.set('n', '<leader>gl', vim.diagnostic.open_float, { desc = 'Open floating di
 key.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- Ctrl + s to save
-key.set({ 'n', 'i' }, '<C-s>', '<Esc>:w<cr>', { noremap = true, silent = true, desc = 'Save' })
+key.set({ 'n', 'i' }, '<C-s>', '<cmd>write<cr>', { noremap = true, silent = true, desc = 'Save' })
 
 -- Neotree
-key.set('n', '<leader>e', ':Neotree<cr>', { desc = 'Op[e]n Neotree' })
+key.set('n', '<leader>e', '<cmd>Neotree<cr>', { desc = 'Op[e]n Neotree' })
+
 -- nvim-notify
 
-key.set('n', '<leader>nf', ':Telescope notify<cr>', { desc = '[N]oti[f]y History' })
+key.set('n', '<leader>nf', '<cmd>Telescope notify<cr>', { desc = '[N]oti[f]y History' })
+
 -- Colorschme
-key.set({ 'n', 'v' }, '<leader>s', '<Esc>:Telescope colorscheme<cr>', { noremap = true, silent = true, desc = 'Choose a colorschme' })
+
+key.set({ 'n', 'v' }, '<leader>s', '<cmd>Telescope colorscheme<cr>', { noremap = true, silent = true, desc = 'Choose a colorschme' })
--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -11,40 +11,6 @@ return {
 
   config = function()
     --  This function gets run when an LSP connects to a particular buffer.
-    local on_attach = function(_, bufnr)
-      -- In this case, we create a function that lets us more easily define mappings specific
-      -- for LSP related items. It sets the mode, buffer and description for us each time.
-
-      local nmap = function(keys, func, desc)
-        if desc then
-          desc = 'LSP: ' .. desc
-        end
-
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-      end
-
-      nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-      nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-      nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-      nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-      nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-      nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-      nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-      nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-      -- See `:help K` for why this keymap
-      nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-      nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-      -- Lesser used LSP functionality
-      nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-      nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-      nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-      nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, '[W]orkspace [L]ist Folders')
-    end
     require('mason').setup {
       ui = {
         check_outdated_packages_on_open = true,
@@ -94,7 +60,6 @@ return {
       function(server_name)
         require('lspconfig')[server_name].setup {
           capabilities = capabilities,
-          on_attach = on_attach,
           settings = servers[server_name],
           filetypes = (servers[server_name] or {}).filetypes,
         }
