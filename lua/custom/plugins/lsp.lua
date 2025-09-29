ff1cae38342cedc161e968dc02832163a24c0aca
shellheim <116480892+shellheim@users.noreply.github.com>
2025-09-29 14:01:15 +0530

    Update config and tidy up

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -3,8 +3,8 @@ return {
   lazy = false,
   dependencies = {
     -- Automatically install LSPs to stdpath for neovim
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
+    'mason-org/mason.nvim',
+    'mason-org/mason-lspconfig.nvim',
     -- Additional lua configuration, makes nvim stuff amazing!
     'folke/neodev.nvim',
   },
